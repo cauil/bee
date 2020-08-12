50ab15d4ded942199bd7ae2c3392be563c750939
cauil <xiaoloujun@gmail.com>
2020-08-13 04:14:07 +0800

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button/button'
+import './styles/index.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <Button>Hello</Button>
+          <Button disabled>Hello</Button>
+          <Button onClick={() => {alert('123')}}>Hello</Button>
+        </div>
+        <div>
+          <Button btnType="primary">Hello</Button>
+          <Button btnType="success">Hello</Button>
+          <Button btnType="warning">Hello</Button>
+          <Button btnType="danger">Hello</Button>
+          <Button btnType="link" href="http://www.baidu.com" target="_blank">Hello</Button>
+          <Button btnType="link" href="http://www.baidu.com" disabled>Hello</Button>
+        </div>
+        <div>
+          <Button size="lg">Hello</Button>
+          <Button>Hello</Button>
+          <Button size="sm">Hello</Button>
+        </div>
+        <code>
+          const a = 1
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
